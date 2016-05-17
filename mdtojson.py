1637786d99a079da21b8ddf0df99503c2b4654ab
bhargav <bhargav.rao448@gmail.com>
2016-05-17 23:44:36 +0530

    added the conversion script

---
+++ mdtojson.py
@@ -0,0 +1,27 @@
+import urllib2
+import json
+import requests
+import re
+
+def get_dict(qurl,desc):
+    qid = qurl.split('/')[4]
+    url = "https://api.stackexchange.com/2.2/questions/{}?site=stackoverflow".format(qid)
+    response = requests.get(url)
+    data = (json.loads(response.text))
+    return {"question": data['items'][0]['link'],
+            "tags": data['items'][0]['tags'],
+            "title": data['items'][0]['title'],
+            "desc": desc}
+
+
+def get_Json():
+    giturl = "https://raw.githubusercontent.com/r-public/HammeR/master/Canonicals.md"
+    response = requests.get(giturl)
+    list_of_data = (response.text.split("\n\n"))
+    list_of_split = [re.match(r'\[(.*?)\]\((.*?)\) (.*)',i).groups() for i in list_of_data]
+    
+    list_of_canons  = [get_dict(i,j) for _,i,j in list_of_split]
+    with open("Canonicals.json", 'wb') as outfile:
+        json.dump(list_of_canons, outfile)
+
+get_Json()
